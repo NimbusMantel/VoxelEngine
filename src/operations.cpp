6dd4e63a9184e17cd1239137326eb6c8a78ea1d2
NimbusMantel <nimbusmantelgames@gmail.com>
2018-05-07 20:12:21 +0100

    Operation container creation

--- src/operations.cpp
+++ src/operations.cpp
@@ -1,1 +1,115 @@
 #include "operations.hpp"
+
+#include <cstdlib>
+
+#define serialiseUint24(memory, offset, data, mask) memory[offset] = data >> 16; memory[offset + 1] = data >> 8; memory[offset + 2] = data & mask;
+#define serialiseUint32(memory, offset, data) memory[offset] = data >> 24;  memory[offset + 1] = data >> 16; memory[offset + 2] = data >> 8; memory[offset + 3] = data;
+
+BUF_END_S::BUF_END_S() {
+
+}
+
+STR_CLD_M::STR_CLD_M(uint24_t address, uint8_t* masks) {
+	serialiseUint24(data, 0, address, 0xF8);
+
+	memcpy(data + 3, masks, 8);
+}
+
+VOX_CPY_M::VOX_CPY_M(uint24_t address, uint24_t* destinations) {
+	serialiseUint24(data, 0, address, 0xF8);
+
+	for (int i = 0; i < 8; i++) {
+		serialiseUint24(data, 3 + i * 3, destinations[i], 0xFF);
+	}
+}
+
+VOX_CPY_S::VOX_CPY_S(uint24_t address, uint24_t destination) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint24(data, 3, destination, 0xFF);
+}
+
+STR_LOA_M::STR_LOA_M(uint24_t address, uint32_t* structures) {
+	serialiseUint24(data, 0, address, 0xF8);
+
+	for (int i = 0; i < 8; i++) {
+		serialiseUint32(data, 3 + i * 4, structures[i]);
+	}
+}
+
+STR_CLD_S::STR_CLD_S(uint24_t address, uint8_t mask) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	data[4] = mask;
+}
+
+STR_PNT_S::STR_PNT_S(uint24_t address, uint24_t pointer) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint24(data, 3, pointer, 0xFF);
+}
+
+STR_LOA_S::STR_LOA_S(uint24_t address, uint32_t structure) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, structure);
+}
+
+MAT_LOA_M::MAT_LOA_M(uint24_t address, uint32_t* materials) {
+	serialiseUint24(data, 0, address, 0xF8);
+
+	for (int i = 0; i < 32; i++) {
+		serialiseUint32(data, 3 + i * 4, materials[i]);
+	}
+}
+
+MAT_xxL_S::MAT_xxL_S(uint24_t address, uint32_t light) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, light);
+}
+
+MAT_xSx_S::MAT_xSx_S(uint24_t address, uint32_t surface) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, surface);
+}
+
+MAT_xSL_S::MAT_xSL_S(uint24_t address, uint32_t surface, uint32_t light) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, surface);
+	serialiseUint32(data, 7, light);
+}
+
+MAT_Cxx_S::MAT_Cxx_S(uint24_t address, uint32_t* colour) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, colour[0]);
+	serialiseUint32(data, 7, colour[1]);
+}
+
+MAT_CxL_S::MAT_CxL_S(uint24_t address, uint32_t* colour, uint32_t light) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, colour[0]);
+	serialiseUint32(data, 7, colour[1]);
+	serialiseUint32(data, 11, light);
+}
+
+MAT_CSx_S::MAT_CSx_S(uint24_t address, uint32_t* colour, uint32_t surface) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, colour[0]);
+	serialiseUint32(data, 7, colour[1]);
+	serialiseUint32(data, 11, surface);
+}
+
+MAT_LOA_S::MAT_LOA_S(uint24_t address, uint32_t* colour, uint32_t surface, uint32_t light) {
+	serialiseUint24(data, 0, address, 0xFF);
+
+	serialiseUint32(data, 3, colour[0]);
+	serialiseUint32(data, 7, colour[1]);
+	serialiseUint32(data, 11, surface);
+	serialiseUint32(data, 15, light);
+}
--- src/operations.hpp
+++ src/operations.hpp
@@ -1,73 +1,82 @@
 #pragma once
 
 #include <cstdint>
+#include <limits>
+
+typedef uint32_t uint24_t;
 
 template<uint8_t opcode, size_t size, uint8_t amount> struct Operation {
-	static const uint8_t opc = opcode;
-	static const size_t siz = size;
-	static const uint8_t amt = amount;
+	private:
+		static const uint8_t opc = opcode;
+		static const size_t siz = size;
+		static const uint8_t amt = amount;
+
+	protected:
+		uint8_t* const data = new uint8_t[siz];
+
+		Operation() = default;
 };
 
-struct BUF_END : Operation<0b0000, 0, std::numeric_limits<uint8_t>::max()>{
-
+struct BUF_END_S : Operation<0b0000, 0, std::numeric_limits<uint8_t>::max()> {
+	BUF_END_S();
 };
 
-struct VOX_LOC : Operation<0b0001, 3 + 8 * ((3 + 1) + (8 + 4 + 4)), 8> {
-
+struct STR_CLD_M : Operation<0b0001, 3 + 8 * 1, 8> {
+	STR_CLD_M(uint24_t address, uint8_t* masks);
 };
 
-struct VOX_COL : Operation<0b0010, 3 + (3 + 8), 1> {
-
+struct VOX_CPY_M : Operation<0b0010, 3 + 8 * 3, 8> {
+	VOX_CPY_M(uint24_t address, uint24_t* destinations);
 };
 
-struct VOX_CPY : Operation<0b0011, 3 + 3, 1> {
-
+struct VOX_CPY_S : Operation<0b0011, 3 + 3, 1> {
+	VOX_CPY_S(uint24_t address, uint24_t destination);
 };
 
-struct STR_LOC : Operation<0b0100, 3 + 8 * (3 + 1), 8> {
-
+struct STR_LOA_M : Operation<0b0100, 3 + 8 * (3 + 1), 8> {
+	STR_LOA_M(uint24_t address, uint32_t* structures);
 };
 
-struct STR_LOS : Operation<0b0101, 3 + (3 + 1), 1> {
-
+struct STR_CLD_S : Operation<0b0101, 3 + 1, 1> {
+	STR_CLD_S(uint24_t address, uint8_t mask);
 };
 
-struct STR_CLD : Operation<0b0110, 3 + 1, 1> {
-
+struct STR_PNT_S : Operation<0b0110, 3 + 3, 1> {
+	STR_PNT_S(uint24_t address, uint24_t pointer);
 };
 
-struct STR_MOV : Operation<0b0111, 3 + 3, 1> {
-
+struct STR_LOA_S : Operation<0b0111, 3 + (3 + 1), 1> {
+	STR_LOA_S(uint24_t address, uint32_t structure);
 };
 
-struct MAT_LOC : Operation<0b1000, 3 + 8 * (8 + 4 + 4), 8> {
-
+struct MAT_LOA_M : Operation<0b1000, 3 + 8 * (8 + 4 + 4), 8> {
+	MAT_LOA_M(uint24_t address, uint32_t* materials);
 };
 
-struct MAT_xxL : Operation<0b1001, 3 + 4, 1> {
-
+struct MAT_xxL_S : Operation<0b1001, 3 + 4, 1> {
+	MAT_xxL_S(uint24_t address, uint32_t light);
 };
 
-struct MAT_xSx : Operation<0b1010, 3 + 4, 1> {
-
+struct MAT_xSx_S : Operation<0b1010, 3 + 4, 1> {
+	MAT_xSx_S(uint24_t address, uint32_t surface);
 };
 
-struct MAT_xSL : Operation<0b1011, 3 + (4 + 4), 1> {
-
+struct MAT_xSL_S : Operation<0b1011, 3 + (4 + 4), 1> {
+	MAT_xSL_S(uint24_t address, uint32_t surface, uint32_t light);
 };
 
-struct MAT_Cxx : Operation<0b1100, 3 + 8, 1> {
-
+struct MAT_Cxx_S : Operation<0b1100, 3 + 8, 1> {
+	MAT_Cxx_S(uint24_t address, uint32_t* colour);
 };
 
-struct MAT_CxL : Operation<0b1101, 3 + (8 + 4), 1> {
-
+struct MAT_CxL_S : Operation<0b1101, 3 + (8 + 4), 1> {
+	MAT_CxL_S(uint24_t address, uint32_t* colour, uint32_t light);
 };
 
-struct MAT_CSx : Operation<0b1110, 3 + (8 + 4), 1> {
-
+struct MAT_CSx_S : Operation<0b1110, 3 + (8 + 4), 1> {
+	MAT_CSx_S(uint24_t address, uint32_t* colour, uint32_t surface);
 };
 
-struct MAT_CSL : Operation<0b1111, 3 + (8 + 4 + 4), 1> {
-
+struct MAT_LOA_S : Operation<0b1111, 3 + (8 + 4 + 4), 1> {
+	MAT_LOA_S(uint24_t address, uint32_t* colour, uint32_t surface, uint32_t light);
 };
